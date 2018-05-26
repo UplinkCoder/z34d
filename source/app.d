403c95d67a11795df6d23700dd4824f8f558131e
Stefan Koch <uplink.coder@googlemail.com>
2018-05-26 12:31:17 +0200

    Add demorgan example from official examples

--- source/app.d
+++ source/app.d
@@ -5,6 +5,20 @@ import z3;
 void main()
 {
 	display_version();
+	demorgan();
+}
+
+
+Z3_solver mk_solver(Z3_context ctx)
+{
+  Z3_solver s = Z3_mk_solver(ctx);
+  Z3_solver_inc_ref(ctx, s);
+  return s;
+}
+
+void del_solver(Z3_context ctx, Z3_solver s)
+{
+  Z3_solver_dec_ref(ctx, s);
 }
 
 void display_version()
@@ -44,4 +58,67 @@ void check(Z3_context ctx, Z3_solver s, Z3_lbool expected_result)
 	exit(0);
     }
     if (m) Z3_model_dec_ref(ctx, m);
+}
+
+/**
+  Demonstration of how Z3 can be used to prove validity of
+  De Morgan's Duality Law: {e not(x and y) <-> (not x) or ( not y) }
+*/
+void demorgan()
+{
+    Z3_config cfg;
+    Z3_context ctx;
+    Z3_solver s;
+    Z3_sort bool_sort;
+    Z3_symbol symbol_x, symbol_y;
+    Z3_ast x, y, not_x, not_y, x_and_y, ls, rs, conjecture, negated_conjecture;
+    Z3_ast[2] args;
+
+    printf("\nDeMorgan\n");
+
+    cfg                = Z3_mk_config();
+    Z3_set_param_value(cfg, "proof", "true");
+    ctx                = Z3_mk_context(cfg);
+    Z3_del_config(cfg);
+    bool_sort          = Z3_mk_bool_sort(ctx);
+    symbol_x           = Z3_mk_string_symbol(ctx, "x");
+    symbol_y           = Z3_mk_string_symbol(ctx, "y");
+    x                  = Z3_mk_const(ctx, symbol_x, bool_sort);
+    y                  = Z3_mk_const(ctx, symbol_y, bool_sort);
+
+    /* De Morgan - with a negation around */
+    /* !(!(x && y) <-> (!x || !y)) */
+    not_x              = Z3_mk_not(ctx, x);
+    not_y              = Z3_mk_not(ctx, y);
+    args[0]            = x;
+    args[1]            = y;
+    x_and_y            = Z3_mk_and(ctx, 2, &args[0]);
+    ls                 = Z3_mk_not(ctx, x_and_y);
+    args[0]            = not_x;
+    args[1]            = not_y;
+    rs                 = Z3_mk_or(ctx, 2, &args[0]);
+    conjecture         = Z3_mk_iff(ctx, ls, rs);
+    negated_conjecture = Z3_mk_not(ctx, conjecture);
+
+    s = mk_solver(ctx);
+    Z3_solver_assert(ctx, s, negated_conjecture);
+
+    final switch (Z3_solver_check(ctx, s)) {
+    case Z3_L_FALSE:
+        /* The negated conjecture was unsatisfiable, hence the conjecture is valid */
+        printf("DeMorgan is valid\n");
+        break;
+    case Z3_L_UNDEF:
+        /* Check returned undef */
+        printf("Undef\n");
+        break;
+    case Z3_L_TRUE:
+        /* The negated conjecture was satisfiable, hence the conjecture is not valid */
+        printf("DeMorgan is not valid\n");
+        break;
+    }
+    //auto m = Z3_solver_get_proof(ctx, s);
+    printf("%s\n", Z3_ast_to_string(ctx, conjecture));
+    del_solver(ctx, s);
+    Z3_del_context(ctx);
 }
